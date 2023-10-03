d31f50ee391297c28c087aafbafff1a5b832c94b
Tilda Källström <tildakallstrom@gmail.com>
2023-10-03 21:40:24 +0200

    Added jquery function to slide h1

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ $(document).ready(function(){
     });
   });
 });
+
+$(document).ready(function(){
+  //animera h1 så den rullar fram över skärmen
+  function startTextAnimation() {
+    $("h1").animate({
+      marginLeft: "-100%" // Flytta texten åt vänster
+    }, 6000, "linear", function() {
+      // Återställ texten när animationen är klar
+      $(this).css("marginLeft", "100%");
+      startTextAnimation(); // Börja om animationen
+    });
+  }
+  startTextAnimation();
+});
